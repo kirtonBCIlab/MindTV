b70f3b6eaab9235efc44c71d673515a1f5743871
ekinney-lang <ekinneyL44@gmail.com>
2024-01-18 12:29:16 -0700

    Added connection functionality 1

--- MindTV-Unity/Assets/Code/Scripts/HeadsetConnectionManager.cs
+++ MindTV-Unity/Assets/Code/Scripts/HeadsetConnectionManager.cs
@@ -3,35 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using BCIEssentials.Controllers;
+using BCIEssentials.LSLFramework;
 
 public class HeadsetConnectManager : MonoBehaviour
 {
-    public Image headsetStatusIndicator;
-    public TextMeshProUGUI headsetStatusText;
 
-    private bool isHeadsetConnected = false;
+    
+    public bool HeadsetConnected { get; private set; }
+
+    [SerializeField] private Image _headsetStatusIndicator;
+    [SerializeField] private TextMeshProUGUI _headsetStatusText;
+    [SerializeField] private GameObject _bciControllerGO;
+
+    private LSLResponseStream _lslResponseStream;
+
+    private void Awake()
+    {
+        HeadsetConnected = false;
+        _bciControllerGO = GameObject.FindWithTag("BCIController");
+        _lslResponseStream = _bciControllerGO.GetComponent<LSLResponseStream>();
+    }
+
 
     // Call this method when the Headset Connect button is clicked
     public void ButtonHeadsetConnection()
     {
-        isHeadsetConnected = !isHeadsetConnected;
-        UpdateHeadsetConnectionStatus();
+        Debug.Log("Attempting to connect to stream...");
+            UpdateHeadsetConnectionStatus();
+    }
+
+    public void SubscribeLSLResponseStream()
+    {
+        //This is depricated behavior
+        _lslResponseStream.Connect();
     }
 
     // Update the headset connection status UI
     private void UpdateHeadsetConnectionStatus()
     {
-        if (isHeadsetConnected)
+        if (_lslResponseStream.Connected)
         {
-            headsetStatusIndicator.color = Color.green; // Unity's predefined green color
-            headsetStatusText.text = "Connected";
-            headsetStatusText.color = Color.green; // Text color changed to green
+            _headsetStatusIndicator.color = Color.green; // Unity's predefined green color
+            _headsetStatusText.text = "Connected";
+            _headsetStatusText.color = Color.green; // Text color changed to green
         }
         else
         {
-            headsetStatusIndicator.color = Color.red; // Unity's predefined red color
-            headsetStatusText.text = "Not Connected";
-            headsetStatusText.color = Color.red; // Text color changed to red
+            _headsetStatusIndicator.color = Color.red; // Unity's predefined red color
+            _headsetStatusText.text = "Not Connected";
+            _headsetStatusText.color = Color.red; // Text color changed to red
         }
     }
 }
